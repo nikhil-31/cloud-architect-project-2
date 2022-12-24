ed5003bda5476507a799be08290a12cb16ffdbca
nikhil-31 <nkl07ba@gmail.com>
2022-12-24 13:30:29 +0530

    Updated screenshots

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -12,15 +12,17 @@ resource "aws_instance" "Udacity_T2" {
   ami = "ami-0323c3dd2da7fb37d"
   instance_type = "t2.micro"
   tags = {
-    name = "Udacity Terraform"
+    Name = "Udacity T2"
+    Environment = "Production"
   }
 }
 
-# resource "aws_instance" "Udacity_M4" {
-#   count = "2"
-#   ami = "ami-0323c3dd2da7fb37d"
-#   instance_type = "m4.large"
-#   tags = {
-#     name = "Udacity Terraform"
-#   }
-# }
+resource "aws_instance" "Udacity_M4" {
+  count = "2"
+  ami = "ami-0323c3dd2da7fb37d"
+  instance_type = "m4.large"
+  tags = {
+    Name = "Udacity M4"
+    Environment = "Production"
+  }
+}
